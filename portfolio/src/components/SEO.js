9ca5d6cdc2d3dafb5d09ddf93236e09590608f63
Krushi Raj Tula <krushiraj123@gmail.com>
2019-08-13 18:47:35 +0530

    commit

--- portfolio/src/components/SEO.js
+++ portfolio/src/components/SEO.js
@@ -78,7 +78,6 @@ const query = graphql`
     site {
       siteMetadata {
         defaultTitle: title
-        titleTemplate
         defaultDescription: description
         siteUrl: url
         defaultImage: image
