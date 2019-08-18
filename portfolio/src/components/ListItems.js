aef5f4236163aca5b9218f5a72f3542a9a57fb91
Krushi Raj Tula <krushiraj123@gmail.com>
2019-08-18 12:14:25 +0530

    Smooth move underline
    Added smooth move underline feature for nav items

--- portfolio/src/components/ListItems.js
+++ portfolio/src/components/ListItems.js
@@ -25,35 +25,73 @@ export default class ListItems extends React.Component {
     this.underLine = React.createRef()
   }
 
+  componentDidMount() {
+    window.addEventListener("resize", this.resizeFunc)
+  }
+
+  resizeFunc = (e, self = this) => {
+    console.log(self)
+    const active = document.querySelector("#nav ul li.active")
+    const target = self.underLine.current
+
+    console.log(active, target)
+
+    if (active) {
+      const left = active.getBoundingClientRect().left + window.pageXOffset
+      const top = active.getBoundingClientRect().top + window.pageYOffset
+
+      target.style.left = `${left}px`
+      target.style.top = `${top}px`
+    }
+  }
+
+  clickHandler = (e, self = this) => {
+    const selectedLi = e.target.parentNode
+    const listItems = document.querySelectorAll("#nav ul li")
+    const target = self.underLine.current
+    if (!selectedLi.classList.contains("active")) {
+      for (let i = 0; i < listItems.length; i++) {
+        if (listItems[i].classList.contains("active"))
+          listItems[i].classList.remove("active")
+      }
+      selectedLi.classList.add("active")
+
+      const width = selectedLi.getBoundingClientRect().width
+      const height = selectedLi.getBoundingClientRect().height
+      const left = selectedLi.getBoundingClientRect().left + window.pageXOffset
+      const top = selectedLi.getBoundingClientRect().top + window.pageYOffset
+
+      target.style.width = `${width}px`
+      target.style.height = `${height}px`
+      target.style.left = `${left}px`
+      target.style.top = `${top - 2}px`
+      target.style.transform = "none"
+    }
+    console.log({
+      target: e.target.parentNode,
+      underline: self.underLine.current,
+      lis: listItems,
+    })
+  }
+
   render() {
     const { top, items } = this.props
     return (
-      <ul style={top ? navTopStyles : navCenterStyles}>
-        {items.map((props, key) => (
-          <ListItem
-            {...props}
-            key={key}
-            clickHandler={(e, self = this) => {
-              console.log({
-                target: e.target.parentNode,
-                underline: this.underLine.current,
-              })
-            }}
-          />
-        ))}
-        <hr
-          style={{
-            margin: 0,
-            background: "red",
-            height: "0px",
-            position: "relative",
-            top: "-3px",
-            border: "none",
-            width: "100px",
-          }}
+      <>
+        <ul style={top ? navTopStyles : navCenterStyles}>
+          {items.map((props, key) => (
+            <ListItem {...props} key={key} clickHandler={this.clickHandler} />
+          ))}
+        </ul>
+        <span
           ref={this.underLine}
+          style={{
+            position: "absolute",
+            borderBottom: "2px solid #e10000",
+            transition: "all 0.3s ease-in-out",
+          }}
         />
-      </ul>
+      </>
     )
   }
 }
