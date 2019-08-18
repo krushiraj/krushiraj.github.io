d65d0d875cf5d0b64b8f95e4c78c3783922197fb
Krushi Raj Tula <krushiraj123@gmail.com>
2019-08-18 11:19:45 +0530

    Deploying to master branch

---
+++ component---src-pages-404-js-6cb2f4bce123444640f3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{193:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a);n.default=function(){return r.a.createElement("div",null,r.a.createElement("h1",null,"404 - Not found"),r.a.createElement("hr",{style:{margin:0,height:"5px"}}),r.a.createElement("p",null,"The resource that you are searching for is not found."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6cb2f4bce123444640f3.js.map
