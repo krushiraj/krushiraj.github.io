82cc8f5b7abc59ec558ee92339e97afc9940ab0f
Krushi Raj Tula <krushiraj123@gmail.com>
2019-08-20 00:26:26 +0530

    Changed project structure
    portfolio - main portfolio site content (Gatsby)
    terminal-app - mini terminal app (Vue)

---
+++ terminal-app/src/utils/setPath.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+fs.writeFileSync(
+    '../portfolio/public/mini-terminal/index.html',
+    fs.readFileSync('../portfolio/public/mini-terminal/index.html')
+        .toString().replace(/=\//g, '=')
+);
--- terminal/terminal-app/src/utils/setPath.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync(
-    '../../portfolio/public/mini-terminal/index.html',
-    fs.readFileSync('../../portfolio/public/mini-terminal/index.html')
-    .toString().replace(/=\//g, '=')
-);
