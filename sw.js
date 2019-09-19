aba24fda07c62dffa044f1069e1c80fa6b715dfa
Krushi Raj Tula <krushiraj123@gmail.com>
2019-09-19 16:06:49 +0530

    Deploying to master branch

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4135cd6b2ae69296a615.js"
+    "url": "webpack-runtime-d942ec40d39be81f4069.js"
   },
   {
     "url": "app-47bf2b214942d0ef6fd5.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e6f54b526a1b5e48b70a41ba2c9ca010"
+    "revision": "2ac1e27141a6cb4113162b0ae8196a4f"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "a23a1088f5dfd0c10183c3cf9cd705aa"
+    "revision": "c1a556ea61cb42c0679e33d0f44f7818"
   },
   {
     "url": "manifest.webmanifest",
