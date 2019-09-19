a77b7c94ab71b9511ef9c4793c5790b0f516025d
Krushi Raj Tula <krushiraj123@gmail.com>
2019-09-19 10:52:47 +0530

    added linkedin

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import { contactItems } from "../utils/commons"
 import FontAwesomeIconWrapper from "./FontAwesomeIconWrapper"
+import { colors } from "../utils/theme"
 
 const StyledContactMe = styled.div`
   display: flex;
@@ -14,6 +15,12 @@ const StyledContactMe = styled.div`
   div {
     margin: auto 0;
     width: 100%;
+  }
+
+  .resume {
+    a {
+      color: ${colors.skyblue};
+    }
   }
 
   .icon-container {
@@ -53,7 +60,7 @@ class Resume extends React.Component {
   render() {
     return <>
       <br/>
-      <p>
+      <p class="resume">
         You can view my resume by clicking{" "}
         <a
           target="_blank"
@@ -71,6 +78,7 @@ class Resume extends React.Component {
           here
         </a>.
       </p>
+      <div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="krushi-raj-t-aba141b1"><a class="LI-simple-link" href='https://in.linkedin.com/in/krushi-raj-t-aba141b1?trk=profile-badge'>Krushi Raj T.</a></div>
       <br/>
     </>
   }
@@ -81,7 +89,7 @@ export default () =>
     <div>
       <p>Feel free to reach me out at:</p>
       <div className="icon-container">
-        {contactItems.slice(0, 4).map((item, key) => (
+        {contactItems.slice(0, 5).map((item, key) => (
           <FontAwesomeIconWrapper key={key} {...item} size="3x" />
         ))}
       </div>
--- portfolio/src/utils/commons.js
+++ portfolio/src/utils/commons.js
@@ -3,6 +3,7 @@ import {
   faTwitter,
   faGithub,
   faStackOverflow,
+  faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons"
 
 export const contactItems = [
@@ -12,19 +13,24 @@ export const contactItems = [
     title: "Mail",
   },
   {
-    href: "https://twitter.com/krushiraj123",
-    icon: faTwitter,
-    title: "Twitter",
-  },
-  {
     href: "https://github.com/krushiraj",
     icon: faGithub,
     title: "GitHub",
   },
   {
+    href: "https://www.linkedin.com/in/krushi-raj-t-aba141b1/",
+    icon: faLinkedinIn,
+    "title": "LinkedIn",
+  },
+  {
     href: "https://stackoverflow.com/users/story/4813058",
     icon: faStackOverflow,
     title: "StackOverflow",
+  },
+  {
+    href: "https://twitter.com/krushiraj123",
+    icon: faTwitter,
+    title: "Twitter",
   },
   {
     href: "https://krushiraj.github.io/rss.xml",
